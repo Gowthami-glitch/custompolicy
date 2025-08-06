1f7fbf8bfd27f96cc7cb3d97355f59ed3b4d0bdb
Gowthami-glitch <gowthami.u174@gmail.com>
2025-08-06 00:48:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,10 @@ terraform {
 
 provider "azurerm" {
   features {}
- subscription_id = "yoursubscriptionid"
+ subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 # Resource Group where the policy will be scoped
